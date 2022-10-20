f614bcd63080deedf62dad1666c11ded64c93313
Pakize Bozkurt <pakiozihak@gmail.com>
2022-10-20 10:15:32 +0100

    Fetching

--- challenges/dog-photo-gallery/dogpics.js
+++ challenges/dog-photo-gallery/dogpics.js
@@ -1,12 +1,35 @@
-fetch("https://dog.ceo/api/breeds/image/random")
-.then(response => response.json())
-.then(res => {
-const dogUl = document.createElement("ul");
-const dogLi = document.createElement("li");
-dogLi.appendChild(dogUl);
-const imgUl = document.createElement("img");
-imgUl.src = res.message;
-})
+let button = document.createElement("button");
+let ul = document.createElement("ul");
+let li = document.createElement("li");
+let img = document.createElement("img");
 
+ul.appendChild(li);
+ul.appendChild(button);
+document.body.appendChild(ul);
 
+li.appendChild(img);
 
+button.setAttribute("id", "btn");
+ul.setAttribute("id", "ul");
+li.setAttribute("id", "li");
+img.setAttribute("id", "imgs");
+
+button.textContent = "Dog Pictures!";
+
+let buttoN_1 = document.getElementById("btn");
+button.addEventListener("click", () => {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then(function (response) {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      } else {
+        throw new Error(
+          `Encountered something unexpected: ${response.status} ${response.statusText}`
+        );
+      }
+    })
+
+    .then(function (image) {
+      img.src = image.message;
+    });
+});
